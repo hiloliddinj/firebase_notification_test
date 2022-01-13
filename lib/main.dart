75ed2cb5138726c3f50102fd3d005dfca181be2e
Hiloliddin <hiloliddin.j@gmail.com>
2022-01-13 19:54:40 +0500

    Foreground, backgroud(app is live just is at bacl) and terminated state firebase notification working

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_notification_test/red_page.dart';
 import 'package:firebase_notification_test/green_page.dart';
-void main() {
+
+Future<void> backgroundHandler(RemoteMessage message) async {
+  print(message.data.toString());
+  print(message.notification!.title);
+}
+
+
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(backgroundHandler);
   runApp(const MyApp());
 }
 
@@ -33,6 +46,38 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    ///Gives you the message on which user taps and it opened the app from terminated state
+    FirebaseMessaging.instance.getInitialMessage().then((message) {
+      if (message != null) {
+        final String routeFromMessage = message.data['route'];
+        print(routeFromMessage);
+        Navigator.of(context).pushNamed(routeFromMessage);
+      }
+
+    });
+
+    ///Foregound work
+    FirebaseMessaging.onMessage.listen((message) {
+      if (message.notification != null) {
+        print(message.notification!.body);
+        print(message.notification!.title);
+      }
+
+    });
+
+    ///Background  but app is open state: user taps on notification
+    FirebaseMessaging.onMessageOpenedApp.listen((message) {
+
+      final String routeFromMessage = message.data['route'];
+      print(routeFromMessage);
+      Navigator.of(context).pushNamed(routeFromMessage);
+    });
+  }
 
 
   @override
